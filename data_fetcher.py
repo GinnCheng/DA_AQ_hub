ba20fd6d1b718b1c00ac42639a145f39740e3c74
GinnCheng <ginn.z.cheng@gmail.com>
2024-11-21 14:08:52 +1100

    feat: nsw AQMS

--- data_fetcher.py
+++ data_fetcher.py
@@ -254,3 +254,242 @@ class qld_amns_data_fetcher:
         else:
             print("period should be one of ('year','24hr','1hr')")
             return None
+
+
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import io
+
+
+class nsw_amns_data_fetcher:
+    def __init__(self, years, data_dir, aqms_name):
+        self.years = years
+        self.data_dir = data_dir
+        self.aqms_name = aqms_name
+        self.df = pd.DataFrame()
+        ######### this part need to be upgraded in the future to web scrapping
+        for year in years[:-1]:
+            temp_data_source = data_dir + aqms_name + '_' + str(year) + '-' + str(year + 1) + '.xls'
+            temp_data = pd.read_excel(temp_data_source, sheet_name='worksheet1', header=2)
+            self.df = pd.concat([self.df, temp_data], axis=0, join='outer')
+        self.df.drop_duplicates(inplace=True)
+        # format time
+        self.df['Date'] = pd.to_datetime(self.df['Date'], format='%d/%m/%Y')
+        self.df['Time'] = self.df['Time'].replace('24:00', '00:00')
+        self.df['Time'] = pd.to_datetime(self.df['Time'], format='%H:%M')
+        self.df['hour'] = self.df['Time'].dt.hour
+        self.df['year'] = self.df.Date.dt.year
+        self.df['month'] = self.df.Date.dt.month
+
+        return self
+
+    # def fetch(self, print_result=False, output=False):
+
+    #     if output == True:
+    #         return self.df
+    #     else:
+    #         return self
+
+    def wrangle(self, quantity_group=[], output=False):
+        # rename all column names to be better called
+        ## this is a wrangling process the format the column names and convert all units into the standard ones
+        ## ALBION PARK SOUTH WDR 1h average [°]
+        ## ALBION PARK SOUTH TEMP 1h average [°C]
+        ## ALBION PARK SOUTH WSP 1h average [m/s]
+        ## ALBION PARK SOUTH NO2 1h average [pphm]
+        ## ALBION PARK SOUTH PM10 1h average [µg/m³]
+        ## ALBION PARK SOUTH PM2.5 1h average [µg/m³]
+        ## ALBION PARK SOUTH HUMID 1h average [%]
+        ## ALBION PARK SOUTH RAIN 1h average [mm/m²]
+
+        # Dictionary for replacements
+        replacements = {
+            'WDR': 'wdr',
+            'WSP': 'wsp',
+            'TEMP': 'temp',
+            'NO2': 'NO2',
+            'PM10': 'PM10',
+            'PM2.5': 'PM2.5',
+            'HUMID': 'hum',
+            'RAIN': 'rain'
+        }
+
+        # Replace column names
+        self.df.columns = [col.replace(old, new) for col in self.df.columns for old, new in replacements.items()]
+
+        ####################################################################################################################################
+        ## convert the unit into standard
+        # convert the unit for WSP from m/s to km/h
+        try:
+            self.df['wsp'] = self.df['wsp'] * 3.6
+        except:
+            pass
+        # convert the unit for NO from ppm to ug/m3
+        try:
+            self.df['NO'] = self.df['NO'] * 1248
+        except:
+            pass
+        # convert the unit for NO2 from pphm to ug/m3
+        try:
+            self.df['NO2'] = self.df['NO2'] * 1
+        except:
+            pass
+        # convert the unit for NOx from ppm to ug/m3
+        try:
+            self.df['NOx'] = self.df['NOx'] * 1311.2  # based on the proportion in the air
+        except:
+            pass
+        # SO2
+        try:
+            self.df['SO2'] = self.df['SO2'] * 2620
+        except:
+            pass
+            # 'nitric_oxide'
+        try:
+            self.df['nitric_oxide'] = self.df['nitric_oxide'] * 1248
+        except:
+            pass
+            # Ozone
+        try:
+            self.df['O_3'] = self.df['O_3'] * 214
+        except:
+            pass
+            # Benzene
+        try:
+            self.df['Benzene'] = self.df['Benzene'] * 3.2
+        except:
+            pass
+            # Toluene
+        try:
+            self.df['Toluene'] = self.df['Toluene'] * 3.77
+        except:
+            pass
+            # Xylenes
+        try:
+            self.df['Xylenes'] = self.df['Xylenes'] * 4.34
+        except:
+            pass
+            # 'Fromaldehyde'
+        try:
+            self.df['Fromaldehyde'] = self.df['Fromaldehyde'] * 1.23
+        except:
+            pass
+
+            ############################################################################################################################
+
+        # check the columns that need to be included in the wrangled data
+        # create a quantity group
+        dust_assess_items_grp = ['NO2', 'SO2', 'PM10', 'PM2.5']
+        voc_assess_items_grp = ['Benzene', 'Toluene', 'Xylenes']
+        wind_rose_items_grp = ['wind_dir', 'wind_spd']
+
+        quantity_dict = {'dust': dust_assess_items_grp, 'voc': voc_assess_items_grp, 'wind': wind_rose_items_grp}
+
+        # select the quantity group that we want to see
+        if quantity_group == []:
+            pass
+        else:
+            quantity_interest = ['Datetime', 'Date', 'Time']
+            for quantity in quantity_group:
+                try:
+                    quantity_interest += quantity_dict[quantity]
+                except KeyError as e:
+                    print(f"Error: The key '{e}' does not exist in the dictionary.\
+                            must choose 1-3 selections from 'dust','voc' and 'wind',\
+                            or you can leave it empty to keep all columns.")
+            quantity_interest = [x for x in quantity_interest if x in self.df.columns]
+
+            self.df = self.df[quantity_interest]
+
+        # drop any NA for the columns that we care about
+        self.df.dropna(subset=self.df.select_dtypes(include=['float']).columns)
+
+        # Function to determine the quarter based on the month
+        def get_season(month):
+            if month in [3, 4, 5]:
+                return 'autumn'
+            elif month in [6, 7, 8]:
+                return 'summer'
+            elif month in [9, 10, 11]:
+                return 'spring'
+            elif month in [12, 1, 2]:
+                return 'winter'
+
+        # wrangle the data
+        self.df['Datetime'] = pd.to_datetime(self.df['Date'] + ' ' + self.df['Time'], format='%d/%m/%Y %H:%M')
+        self.df['year'] = self.df.Datetime.dt.year
+        self.df['month'] = self.df.Datetime.dt.month
+        self.df['day'] = self.df.Datetime.dt.day
+        self.df['hour'] = self.df.Datetime.dt.hour
+        # Apply the function to create a new column for quarters
+        self.df['season'] = self.df['month'].apply(get_season)
+        # df = df.sort_values(by=['Datetime'])
+        self.df = self.df.set_index('Datetime')
+        self.df.sort_index(inplace=True)
+
+        if output == True:
+            return self.df
+        else:
+            return self
+
+    def average(self, period='year'):
+        if period == '24hr':
+            df_avg = self.df.groupby(['year', 'month', 'day'])[
+                self.df.select_dtypes(include='float').columns].mean().reset_index()
+            df_avg_yr100 = df_avg.groupby(['year']).max().reset_index()
+            df_avg_yr90 = df_avg.groupby(['year']).quantile(0.90).reset_index()
+
+            # Rename columns of df2 with suffix '_90'
+            df_avg_yr90 = df_avg_yr90[self.df.select_dtypes(include='float').columns].rename(
+                columns=lambda x: f"{x}_90")
+
+            # Merge DataFrames side by side
+            df_avg = pd.concat([df_avg_yr100, df_avg_yr90], axis=1)
+
+            # Interleave the columns
+            # Create the interleaved column order
+            cols = []
+            for col in df_avg_yr100.columns:
+                cols.append(col)
+                cols.append(f"{col}_90")
+            cols = [col for col in cols if (col in df_avg_yr100.columns or col in df_avg_yr90.columns)]
+            # Reorder the merged DataFrame with the new column order
+            df_avg = df_avg[cols]
+
+            return df_avg.round(2)
+
+        elif period == '1hr':
+            df_avg_yr100 = self.df.groupby(['year'])[self.df.select_dtypes(include='float').columns].max().reset_index()
+            df_avg_yr90 = self.df.groupby(['year'])[self.df.select_dtypes(include='float').columns].quantile(
+                0.90).reset_index()
+
+            # Rename columns of df2 with suffix '_90'
+            df_avg_yr90 = df_avg_yr90[self.df.select_dtypes(include='float').columns].rename(
+                columns=lambda x: f"{x}_90")
+
+            # Merge DataFrames side by side
+            df_avg = pd.concat([df_avg_yr100, df_avg_yr90], axis=1)
+
+            # Interleave the columns
+            # Create the interleaved column order
+            cols = []
+            for col in df_avg_yr100.columns:
+                cols.append(col)
+                cols.append(f"{col}_90")
+            cols = [col for col in cols if (col in df_avg_yr100.columns or col in df_avg_yr90.columns)]
+            # Reorder the merged DataFrame with the new column order
+            df_avg = df_avg[cols]
+
+            return df_avg.round(2)
+
+        elif period == 'year':
+
+            df_avg = self.df.groupby(['year'])[self.df.select_dtypes(include='float').columns].mean().reset_index()
+            return df_avg.round(2)
+
+        else:
+            print("period should be one of ('year','24hr','1hr')")
+            return None
+
+
